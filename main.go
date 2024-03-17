b12c0034c9b2d3ca5c6f277bd6a7c9b12e8d5cb5
Kusuma Sandi <wijayakusumasandi@gmail.com>
2024-03-17 22:00:10 +0800

    feat: 🎸 added core functionality

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "github.com/mrspec7er/gorch/pkg"
+import (
+	"fmt"
+	"time"
+
+	"github.com/mrspec7er/gorch/pkg"
+)
+
+type Dummy struct {
+	ID    uint
+	Label string
+	Value int
+}
 
 func main() {
-	pkg.Intro()
+	data := []Dummy{
+		{ID: 1, Label: "Name", Value: 111},
+		{ID: 2, Label: "Specialty", Value: 222},
+		{ID: 3, Label: "Net Worth", Value: 333},
+	}
+
+	filename := "test_" + time.Now().GoString()
+	pkg.Insert(filename, data)
+
+	time.Sleep(2 * time.Second)
+
+	result := pkg.Find[Dummy](filename, "Value", "222")
+
+	fmt.Println("Result: ", result)
 }
--- pkg/main.go
+++ pkg/main.go
@@ -1,7 +1,74 @@
 package pkg
 
-import "fmt"
+import (
+	"encoding/gob"
+	"fmt"
+	"os"
+	"reflect"
+)
 
-func Intro() {
-	fmt.Println("Integrated search service for your application")
+type Any interface{}
+
+func Store(fileName string) *os.File {
+	file, err := os.Create("data/" + fileName + ".bin")
+	if err != nil {
+		fmt.Println("Error creating file:", err)
+		return nil
+	}
+
+	return file
+}
+
+func Read(fileName string) *os.File {
+	file, err := os.Open("data/" + fileName + ".bin")
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return nil
+	}
+
+	return file
+}
+
+func Insert[S any](filename string, data S) {
+	file := Store(filename)
+	defer file.Close()
+
+	encoder := gob.NewEncoder(file)
+	err := encoder.Encode(data)
+	if err != nil {
+		fmt.Println("Error encoding map to binary:", err)
+		return
+	}
+}
+
+func Find[S any](filename string, key string, value any) any {
+	file := Read(filename)
+	defer file.Close()
+
+	decoder := gob.NewDecoder(file)
+	var data []S
+	err := decoder.Decode(&data)
+	if err != nil {
+		fmt.Println("Error decoding binary data:", err)
+		return nil
+	}
+
+	for _, d := range data {
+		val := reflect.ValueOf(d)
+		if val.Kind() != reflect.Struct {
+			fmt.Println("Error: input data is not a struct")
+			return nil
+		}
+		fieldType := val.FieldByName(key).Type()
+		if fieldType != reflect.TypeOf(value) {
+			fmt.Println("Field type doesn't match value type")
+			continue
+		}
+		fieldValue := val.FieldByName(key).Interface()
+		if fieldValue == value {
+			return d
+		}
+	}
+
+	return nil
 }
